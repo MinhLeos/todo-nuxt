d5a82ddca70d0d51215e8aff4604c9fa2ee7d84b
Minh Le <minh.le@mgisolutions.com>
2023-05-11 11:30:20 +0700

    add API to test lazy

---
+++ src/server/api/quote/[id].get.ts
@@ -0,0 +1,18 @@
+// làm chậm thời gian gọi API để test useFetch và useLazyFetch hoặc useAsyncData và useLazyAsyncData
+
+export default defineEventHandler(async (event: any) => {
+    try {
+      const res = await $fetch(`https://636322ac66f75177ea3e0792.mockapi.io/quote/${event.context.params.id}`, {
+        method: "GET",
+      });
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(res);
+        }, 2000);
+      });
+    } catch (error: any) {
+      return {
+        error: error.message,
+      };
+    }
+  });
