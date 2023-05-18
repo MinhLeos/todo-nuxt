ded42e7c84651ee1edeed142839bb8e8aa769418
Minh Le <minh.le@mgisolutions.com>
2023-05-18 10:44:05 +0700

    123

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,7 +14,11 @@
 </template>
 
 <script setup>
+
 const title = useState('title')
+const plugin = useState('plugin')
+plugin.value = 'Plugin V3'
+console.log('plugin', plugin)
 useHead({
     title: title
 })
---
+++ src/plugins/2.testPlugin.ts
@@ -0,0 +1,3 @@
+export default defineNuxtPlugin(nuxtApp => {
+    const title = useState('plugin', () => 'Plugin V2')
+  })
